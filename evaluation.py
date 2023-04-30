dbf6d8eb723fc0b39ba704032c17f37c2a583527
s408045 <alexander.huhn@stud-mail.uni-wuerzburg.de>
2023-04-30 13:15:46 +0200

    folder-wise evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,20 +1,24 @@
 import madmom.features.downbeats as downbeats
 import numpy as np
 import librosa
+import glob
+import warnings
+from numpy import VisibleDeprecationWarning
 
 beats_per_bar = 4
 
 
 def evaluate_rhythm(audio_path):
-    print("path ", audio_path)
 
     # RNNDownBeatProcessor
     act = downbeats.RNNDownBeatProcessor()(audio_path)
     # DBNDownBeatTrackingProcessor
-    proc2 = downbeats.DBNDownBeatTrackingProcessor(beats_per_bar=[beats_per_bar, beats_per_bar], fps=100, correct=False)
+    processor = downbeats.DBNDownBeatTrackingProcessor(beats_per_bar=[beats_per_bar, beats_per_bar], fps=100, correct=False)
     # fps=100 because of RNNDownBeatProcessor return,
     # correct=False because this is for evaluation hence aligning the beats to the nearest peak would be wrong
-    result = proc2(act)
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=VisibleDeprecationWarning) # ignore warning caused by madmom
+        result = processor(act)
     # length of the track (required later)
     audio_length = librosa.get_duration(filename=audio_path)
 
@@ -29,7 +33,6 @@ def evaluate_rhythm(audio_path):
     for i in range(0, len(rawbeat_std) - 1):
         beat_new[i] = rawbeat_std[i + 1] - rawbeat_std[i]
     beat_std = np.std(beat_new[:-1]) # except in this case the last one, as it is longer due to the generation
-    print(f"Beat Std = {beat_std} \t \t \t values: {beat_new[:-1]}")
 
     # Downbeat Std (standard deviation of the bar length differences):
     rawdownbeat_std = [i[0] for i in result]
@@ -38,16 +41,19 @@ def evaluate_rhythm(audio_path):
     for i in range(1, len(downbeat_new)):
         downbeat_new[i] = rawdownbeat_std[(i + 1) * beats_per_bar] - rawdownbeat_std[i * beats_per_bar]
     downbeat_std = np.std(downbeat_new)
-    print(f"Downbeat Std = {downbeat_std} \t \t \t values: {downbeat_new}")
 
     # Downbeat Salience
     downbeat_salience = [i[1] for i in act]
     salience = np.mean(downbeat_salience)
-    print("Downbeat Salience = ", salience)
 
-    return beat_std, downbeat_std, salience
+    return beat_std, beat_new[:-1], downbeat_std, downbeat_new, salience
 
 
 if __name__ == "__main__":
-    audio_path = './result_synthesized/initial_from_scratch.wav'
-    evaluate_rhythm(audio_path)
+    audio_folder = './result_synthesized/'
+    for audio_path in glob.glob(f'{audio_folder}*.wav'):
+        beat_std, beat_new, downbeat_std, downbeat_new, salience = evaluate_rhythm(audio_path)
+        print(f"path: {audio_path}")
+        print(f"Beat Std = \t \t \t{beat_std}")
+        print(f"Downbeat Std = \t \t{downbeat_std}")
+        print(f"Downbeat Salience = {salience}\n")
