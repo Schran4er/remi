5d73fd64a795c22bbdb4d0cefb229c1eb321da85
s408045 <alexander.huhn@stud-mail.uni-wuerzburg.de>
2023-04-30 14:30:01 +0200

    update evaluation and generation

--- evaluation.py
+++ evaluation.py
@@ -9,15 +9,15 @@ beats_per_bar = 4
 
 
 def evaluate_rhythm(audio_path):
-
     # RNNDownBeatProcessor
     act = downbeats.RNNDownBeatProcessor()(audio_path)
     # DBNDownBeatTrackingProcessor
-    processor = downbeats.DBNDownBeatTrackingProcessor(beats_per_bar=[beats_per_bar, beats_per_bar], fps=100, correct=False)
+    processor = downbeats.DBNDownBeatTrackingProcessor(beats_per_bar=[beats_per_bar, beats_per_bar], fps=100,
+                                                       correct=False)
     # fps=100 because of RNNDownBeatProcessor return,
     # correct=False because this is for evaluation hence aligning the beats to the nearest peak would be wrong
     with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=VisibleDeprecationWarning) # ignore warning caused by madmom
+        warnings.filterwarnings("ignore", category=VisibleDeprecationWarning)  # ignore warning caused by madmom
         result = processor(act)
     # length of the track (required later)
     audio_length = librosa.get_duration(filename=audio_path)
@@ -32,12 +32,13 @@ def evaluate_rhythm(audio_path):
 
     for i in range(0, len(rawbeat_std) - 1):
         beat_new[i] = rawbeat_std[i + 1] - rawbeat_std[i]
-    beat_std = np.std(beat_new[:-1]) # except in this case the last one, as it is longer due to the generation
+    beat_std = np.std(beat_new[:-1])  # except in this case the last one, as it is longer due to the generation
 
     # Downbeat Std (standard deviation of the bar length differences):
     rawdownbeat_std = [i[0] for i in result]
     downbeat_new = [0] * (len(rawdownbeat_std) // beats_per_bar - 1)
-    downbeat_new[0] = rawdownbeat_std[4] - rawdownbeat_std[0]  # because rawdownbeat_std[4] is the time when the first bar ends
+    downbeat_new[0] = rawdownbeat_std[4] - rawdownbeat_std[
+        0]  # because rawdownbeat_std[4] is the time when the first bar ends
     for i in range(1, len(downbeat_new)):
         downbeat_new[i] = rawdownbeat_std[(i + 1) * beats_per_bar] - rawdownbeat_std[i * beats_per_bar]
     downbeat_std = np.std(downbeat_new)
@@ -50,10 +51,27 @@ def evaluate_rhythm(audio_path):
 
 
 if __name__ == "__main__":
-    audio_folder = './result_synthesized/'
+    print("start")
+    audio_folder = './result_synthesized/'  # input folder
+    all_results = [[], [], []]
+    all_results_length = 0
+
     for audio_path in glob.glob(f'{audio_folder}*.wav'):
         beat_std, beat_new, downbeat_std, downbeat_new, salience = evaluate_rhythm(audio_path)
         print(f"path: {audio_path}")
         print(f"Beat Std = \t \t \t{beat_std}")
         print(f"Downbeat Std = \t \t{downbeat_std}")
         print(f"Downbeat Salience = {salience}\n")
+        all_results[0].append(beat_std)
+        all_results[1].append(downbeat_std)
+        all_results[2].append(salience)
+
+    # calculate mean result values
+    a = all_results[0]
+    result_beat_std = np.mean(all_results[0])
+    b = all_results[1]
+    result_downbeat_std = np.mean(all_results[1])
+    c = all_results[2]
+    result_salience = np.mean(all_results[2])
+    print(f"all_results: (mean)\nBeat Std = \t \t \t{result_beat_std}\nDownbeat Std = \t \t{result_downbeat_std}\nDownbeat "
+          f"salience = {result_salience}")
--- main.py
+++ main.py
@@ -4,29 +4,34 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
-def main():
-    # declare model
-    model = PopMusicTransformer(
-        # checkpoint='REMI-tempo-checkpoint',
-        # checkpoint = 'REMI-tempo-chord-checkpoint',
-        checkpoint='my_midi_data_checkpoint',
-        is_training=False)
-
-    # generate from scratch
+# if no prompt given: generate from scratch
+#  otherwise: generate continuation
+def generate(model: PopMusicTransformer, output_path: str, prompt=None):
     model.generate(
         n_target_bar=16,
         temperature=1.2,
         topk=5,
-        output_path='./result/from_scratch.midi',
-        prompt=None)
+        output_path=output_path,
+        prompt=prompt)
+
+
+def main():
+    # declare model
+    model = PopMusicTransformer(
+        checkpoint='REMI-tempo-checkpoint',
+        # checkpoint = 'REMI-tempo-chord-checkpoint',
+        # checkpoint='my_midi_data_checkpoint',
+        is_training=False)
+
+    # generate from scratch
+    for i in range(0, 10):
+        output_path = f'./result/tempo_checkpoint_from_scratch_{i}.midi'
+        generate(model, output_path)
 
     # generate continuation
-    model.generate(
-        n_target_bar=16,
-        temperature=1.2,
-        topk=5,  # added ","
-        output_path='./result/continuation.midi',
-        prompt='./data/evaluation/000.midi')
+    # output_path = './result/continuation.midi'
+    # prompt = './data/evaluation/000.midi'
+    # generate(model, output_path, prompt)
 
     # close model
     model.close()
@@ -34,4 +39,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
