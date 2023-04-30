749237803f062479d91cd79248ba37bec3d7f5e2
s408045 <alexander.huhn@stud-mail.uni-wuerzburg.de>
2023-04-30 12:47:17 +0200

    evaluation re-evaluated

--- evaluation.py
+++ evaluation.py
@@ -1,43 +1,49 @@
 import madmom.features.downbeats as downbeats
 import numpy as np
+import librosa
 
 beats_per_bar = 4
 
 
 def evaluate_rhythm(audio_path):
     print("path ", audio_path)
+
     # RNNDownBeatProcessor
     act = downbeats.RNNDownBeatProcessor()(audio_path)
     # DBNDownBeatTrackingProcessor
-    proc2 = downbeats.DBNDownBeatTrackingProcessor(beats_per_bar=[beats_per_bar, beats_per_bar], fps=100)
+    proc2 = downbeats.DBNDownBeatTrackingProcessor(beats_per_bar=[beats_per_bar, beats_per_bar], fps=100, correct=False)
+    # fps=100 because of RNNDownBeatProcessor return,
+    # correct=False because this is for evaluation hence aligning the beats to the nearest peak would be wrong
     result = proc2(act)
+    # length of the track (required later)
+    audio_length = librosa.get_duration(filename=audio_path)
 
-    # Beat Std (standard deviation of the beat length differences)
+    # Beat Std (standard deviation of the beat length differences):
     rawbeat_std = [i[0] for i in result]
-    beat_new = [0] * (len(rawbeat_std) - 1)
-    beat_new[0] = rawbeat_std[1] - rawbeat_std[0]
-    for i in range(1, len(rawbeat_std) - 1):
-        beat_new[i] = rawbeat_std[i + 1] - rawbeat_std[i]
-    beat_std = np.std(beat_new, ddof=1)
-    print("Beat Std = ", beat_std)
+    beat_new = [0] * len(rawbeat_std)
 
-    # Downbeat Std (standard deviation of the bar length differences)
+    # set length of last element
+    last_element_index = len(rawbeat_std) - 1
+    beat_new[last_element_index] = audio_length - rawbeat_std[last_element_index]
+
+    for i in range(0, len(rawbeat_std) - 1):
+        beat_new[i] = rawbeat_std[i + 1] - rawbeat_std[i]
+    beat_std = np.std(beat_new[:-1]) # except in this case the last one, as it is longer due to the generation
+    print(f"Beat Std = {beat_std} \t \t \t values: {beat_new[:-1]}")
+
+    # Downbeat Std (standard deviation of the bar length differences):
     rawdownbeat_std = [i[0] for i in result]
-    downbeat_new = [0] * (len(rawdownbeat_std) // beats_per_bar)
-    downbeat_new[0] = rawdownbeat_std[4]  # because rawdownbeat_std[4] is the time when the first bar ends
-    for i in range(1, len(rawdownbeat_std) // beats_per_bar):
-        downbeat_new[i] = rawdownbeat_std[(i + 1) * beats_per_bar - 1] - rawdownbeat_std[i * beats_per_bar - 1]
-    downbeat_std = np.std(downbeat_new, ddof=1)
-    print("Downbeat Std = ", downbeat_std)
+    downbeat_new = [0] * (len(rawdownbeat_std) // beats_per_bar - 1)
+    downbeat_new[0] = rawdownbeat_std[4] - rawdownbeat_std[0]  # because rawdownbeat_std[4] is the time when the first bar ends
+    for i in range(1, len(downbeat_new)):
+        downbeat_new[i] = rawdownbeat_std[(i + 1) * beats_per_bar] - rawdownbeat_std[i * beats_per_bar]
+    downbeat_std = np.std(downbeat_new)
+    print(f"Downbeat Std = {downbeat_std} \t \t \t values: {downbeat_new}")
 
     # Downbeat Salience
     downbeat_salience = [i[1] for i in act]
-    salience = sum(downbeat_salience) / len(downbeat_salience)
+    salience = np.mean(downbeat_salience)
     print("Downbeat Salience = ", salience)
-
-    downbeat_salience_0 = [i[0] for i in act]
-    salience_0 = sum(downbeat_salience_0) / len(downbeat_salience_0)
-    print("Downbeat Salience_0 = ", salience_0)
 
     return beat_std, downbeat_std, salience
 
